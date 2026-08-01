ae6210565314be6bed389db91ce4b1b1110a54ff
abdullahsaqibb1 <masaqib12@gmail.com>
2026-08-01 09:32:24 +0500

    Fix storefront blank screen
    Added ScrollToTop component to reset scroll position on route change.

--- apps/storefront/src/components/StoreLayout.tsx
+++ apps/storefront/src/components/StoreLayout.tsx
@@ -1,9 +1,37 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { AnnouncementBar } from './AnnouncementBar';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { CartDrawer } from './CartDrawer';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'auto',
+    });
+  }, [pathname]);
+
+  return null;
+}
+
 export function StoreLayout() {
-  return <><AnnouncementBar /><Header /><main><Outlet /></main><Footer /><CartDrawer /><ScrollRestoration /></>;
+  return (
+    <>
+      <ScrollToTop />
+      <AnnouncementBar />
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+      <CartDrawer />
+    </>
+  );
 }
